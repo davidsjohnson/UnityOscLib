cab6faca0b7134c0f41b2ba9016b3491d263c2b5
David Johnson <fortjay81@gmail.com>
2019-02-14 16:08:50 -0800

    Prep for release to Github
    Implements Control Rate Feature
    Updates Namespace to UnityOscLib
    Reorganized files
    Includes a Test Scene to test sending and receiving through managers
    Inclues an OscMangers prefab

---
+++ Assets/UnityOscLib/Scripts/OSCManagers/OscReceiverManager.cs
@@ -0,0 +1,119 @@
+﻿namespace UnityOscLib
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.Events;
+
+    // Class to support UnityEvents for handling OSCMessages
+    [Serializable]
+    public class UnityEventOscMessage : UnityEvent<OSCMessage> { }
+
+    // Delegate class for OSC address handling
+    public delegate void OscReceivedHandler(OSCMessage message);         
+
+    public class OscReceiverManager : MonoBehaviour
+    {
+
+        // #### Unity Inspetor Properties ####
+        // ###################################
+
+        // Class to allow Unity Inspector Configuration of Event Handling
+        [Serializable]
+        public class OscAddressHandler
+        {
+            public string oscAddress;
+            public UnityEventOscMessage oscMessageHandler = new UnityEventOscMessage();
+
+            public OscAddressHandler() { }
+
+            public OscAddressHandler(string addr)
+            {
+                oscAddress = addr;
+            }
+
+            public OscAddressHandler(string addr, UnityAction<OSCMessage> handler)
+            {
+                oscAddress = addr;
+                oscMessageHandler.AddListener(handler);
+            }
+        }
+
+        public int listeningPort = 10101;
+
+        // #### Class  Properties ####
+        // ###########################
+
+        public static OscReceiverManager Instance { get; private set; }     // Provide access to Receiver singleton
+        private OSCReciever oscReciever = new OSCReciever();                             
+
+        // Add OSC address registration to the Unity Inspector; 
+        // Adding a blank item to add visual cue to user about the usage 
+        // and too provide a default method for handling all messages
+        [SerializeField]
+        private List<OscAddressHandler> oscAddressHandlers = new List<OscAddressHandler>()
+        {
+            new OscAddressHandler("/*"),
+        };
+
+
+
+        void Awake()
+        {
+            // Implement Pseudo-Singleton 
+            // Should only need one receiver per project
+            if (Instance == null)
+            {
+                DontDestroyOnLoad(gameObject);
+                Instance = this;
+            }
+            else if (Instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        private void OnEnable()
+        {
+            oscReciever.Open(listeningPort);
+            StartCoroutine("ProcessMessages");
+        }
+
+        private void OnDisable()
+        {
+            oscReciever.Close();
+            StopCoroutine("ProcessMessages");
+        }
+
+        public void RegisterOscAddress(string oscAddress, UnityAction<OSCMessage> handler)
+        {
+            // Change to dict or tree implementation for better routing capabilities
+            oscAddressHandlers.Add(new OscAddressHandler(oscAddress, handler));
+        }
+
+        private IEnumerator ProcessMessages()
+        {
+            while (true)
+            {
+                while (oscReciever.hasWaitingMessages())
+                {
+                    OSCMessage message = oscReciever.getNextMessage();
+                    foreach (var addrHandler in oscAddressHandlers)
+                    {
+                        if (addrHandler.oscAddress.Equals(message.Address)) addrHandler.oscMessageHandler.Invoke(message);
+                        else if (addrHandler.oscAddress.Equals("/*")) addrHandler.oscMessageHandler.Invoke(message);
+                    }
+                }
+                yield return new WaitForSeconds(.01f);
+            }
+
+        }
+
+        public void DefaultHandler(OSCMessage message)
+        {
+            Debug.Log(string.Format("Message Received {0}", message));
+        }
+
+    }
+}
---
+++ Assets/UnityOscLib/Scripts/OSCManagers/OscTransmitManager.cs
@@ -0,0 +1,130 @@
+﻿namespace UnityOscLib
+{
+
+    using System;
+    using System.Net;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class OscTransmitManager : MonoBehaviour
+    {
+        // #### Unity Inspector Items ####
+        // ###############################
+        [Tooltip("Set the Control Rate for the OnSendOsc event. Setting to 0 used Fixed Update instead of OnSendOsc")]
+        [SerializeField]
+        private int controlRateHz = 0;
+
+        // List of OscReceiverInfo structs to support receiver config
+        // in the Unity Inspector (since it doesn't support dicts)
+        // Struct for Receiver List in Unity Inspector 
+        [Serializable]
+        public class OscReceiverInfo
+        {
+            public string name;
+            public string host;
+            public int port;
+        }
+        [SerializeField]
+        private List<OscReceiverInfo> oscReceivers = new List<OscReceiverInfo>(); 
+
+
+        // #### Class Items ####
+        // #####################
+        public static OscTransmitManager Instance { get; private set; }                                         // Provide access to the Transmitter Singlton
+
+        private readonly Dictionary<string, OSCClient> oscReceiversDict = new Dictionary<string, OSCClient>();  // Store Receiver Configuration
+
+        //Control Rate Delegate and Event
+        public delegate void SendOsc();
+        public event SendOsc OnSendOsc;
+
+        private void Awake()
+        {
+            // Implement Pseudo-Singleton 
+            // Should only need one transmitter per project
+            if (Instance == null)
+            {
+                DontDestroyOnLoad(gameObject);
+                Instance = this;
+            }
+            else if (Instance != this)
+            {
+                Destroy(gameObject);
+            }
+
+            //Initialize Dict from Receivers added in Inspector
+            foreach (var rec in oscReceivers)
+            {
+                oscReceiversDict[rec.name] = new OSCClient(IPAddress.Parse(rec.host), rec.port);
+            }
+        }
+
+        private void Start()
+        {
+            if (controlRateHz > 0)
+            {
+                StartCoroutine("ControlRateUpdate");
+            }
+        }
+
+        private void OnDestroy()
+        {
+            if (controlRateHz > 0)
+            {
+                StopCoroutine("ControlRateUpdate");
+            }
+        }
+
+        private IEnumerator ControlRateUpdate()
+        {
+            Debug.Log("Starting Control Reate Upeate");
+            while (true)
+            {
+                
+                if (OnSendOsc != null)
+                    OnSendOsc.Invoke();
+                yield return new WaitForSecondsRealtime(1.0f / controlRateHz);
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            if (controlRateHz <= 0)
+            {
+                if (OnSendOsc != null)
+                    OnSendOsc.Invoke();
+            }
+        }
+
+        public void AddReceiver(string name, string host, int port)
+        {
+            oscReceivers.Add(new OscReceiverInfo { name = name, host = host, port = port });
+            oscReceiversDict[name] = new OSCClient(IPAddress.Parse(host), port);
+        }
+
+        public void SendOscMessageAll(string addresss, params object[] values)
+        {
+            foreach (var entry in oscReceiversDict)
+            {
+                SendOscMessage(entry.Key, addresss, values);
+            }
+        }
+
+        public void SendOscMessage(string name, string address, params object[] values)
+        {
+            // Validate that address starts with a /
+            address = address[0] != '/' ? '/' + address : address;
+
+            OSCMessage message = new OSCMessage(address);
+            foreach (var msgvalue in values)
+            {
+                message.Append(msgvalue);
+            }
+
+            Debug.Log(string.Format("Sending OSC to Receiver {0}: {2}", name, address, message));
+
+            oscReceiversDict[name].Send(message);
+        }
+    }
+}
